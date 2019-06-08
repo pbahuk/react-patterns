f8eb3312c49fdfd00f71c70225709f713bcd9b38
Prabhat Bahukhandi <prabhat.bahukhandi@myntra.com>
2019-06-08 12:42:16 +0530

    Reach router: 2 routes added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
+
+// Components Import.
+import First from "./components/First";
+import Second from "./components/Second";
 
 const App = () => {
-  return <h1>"Hello World"</h1>;
+  return (
+    <h1>
+      "Hello World"
+      <Router>
+        <First path="/" />
+        <Second path="/second" />
+      </Router>
+    </h1>
+  );
 };
 
 render(<App />, document.getElementById("root"));
---
+++ src/components/First.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const First = () => {
+  return <div>First Route</div>;
+};
+
+export default First;
