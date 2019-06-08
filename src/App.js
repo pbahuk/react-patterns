23fa2985ed7eb87f7aae9e29c5edbc1838427348
Prabhat Bahukhandi <prabhat.bahukhandi@myntra.com>
2019-06-08 12:37:03 +0530

    Lint configuration, .eslintrc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 
-const App = React.createElement("h1", {}, "Hello World !!");
+const App = () => {
+  return <h1>"Hello World"</h1>;
+};
 
-render(App, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
